0ad60b713891a3b4efdad31aa091ed643053ffe6
foxxyiness <coachfoxjp@gmail.com>
2021-03-06 20:59:02 -0500

    Created Victory Scene
    created victory screen with scripts and ui

--- Project Fruit Basket/Assets/Scripts/VictoryScreen.cs
+++ Project Fruit Basket/Assets/Scripts/VictoryScreen.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TitleScreen : MonoBehaviour
+public class VictoryScreen : MonoBehaviour
 {
-    public void StartGame()
+   public void PlayAgain()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
     public void QuitGame()
     {
         Application.Quit();
     }
+
+
 }
